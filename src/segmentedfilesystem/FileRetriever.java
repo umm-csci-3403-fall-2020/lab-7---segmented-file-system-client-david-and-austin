e01d8227139e8f7dbc87250a229abf19243e6a66
David <lucht013@morris.umn.edu>
2020-11-03 10:28:34 -0600

    Fill FileRetriever

--- src/segmentedfilesystem/FileRetriever.java
+++ src/segmentedfilesystem/FileRetriever.java
@@ -1,24 +1,23 @@
 package segmentedfilesystem;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.net.InetAddress;
-import java.net.SocketException;
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.net.*;
 import java.util.Stack;
 
 public class FileRetriever {
 	Stack<DataPacket> data = new Stack<>();
 	int port;
 	String server;
-
+	DatagramSocket socket;
 
 	public FileRetriever(String server, int port) throws SocketException{
         this.port = port;
 		this.server = server;
-		DatagramSocket socket = new DatagramSocket(port);
+		socket = new DatagramSocket(port);
 	}
 
-	public void downloadFiles() {
+	public void downloadFiles() throws UnknownHostException, IOException {
         // Do all the heavy lifting here.
         // This should
         //   * Connect to the server
@@ -33,11 +32,22 @@ public class FileRetriever {
         // ways.
 		boolean allFilesDownloaded = false;
 		InetAddress address = InetAddress.getByName(server);
+		RecievedFile file1 = new RecievedFile();
+		RecievedFile file2 = new RecievedFile();
+		RecievedFile file3 = new RecievedFile();
 		while(allFilesDownloaded == false){
 			byte[] buf = new byte[1028];
 			DatagramPacket freshPacket = new DatagramPacket(buf, buf.length);
-			socket.recieve
+			socket.receive(freshPacket);
+
+			if(freshPacket.getData()[0] % 2 == 0){
+				//it's a headerpacket
+			}else{
+				//it's a data packet
+			}
+			if()
 		}
+
 	}
 
 }
