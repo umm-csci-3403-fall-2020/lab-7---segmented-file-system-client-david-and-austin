ab4acdb5f1b1da1c7b8dfc2d9fa32ebacb88dcc4
Austin Robinson <robi1467@morris.umn.edu>
2020-11-03 10:28:19 -0600

    add counter for packetNumbers

--- src/segmentedfilesystem/RecievedFile.java
+++ src/segmentedfilesystem/RecievedFile.java
@@ -1,17 +1,40 @@
 package segmentedfilesystem;
 
-import java.util.SortedMap;
+import java.util.HashMap;
+
 
 public class RecievedFile {
-    SortedMap map;
-    public RecievedFile(){
+    DataPacket data;
+    HeaderPacket header;
+    Integer numPackets;
 
+
+    HashMap<byte[],DataPacket> datapackets = new HashMap<>();
+
+    public RecievedFile(DataPacket dp, HeaderPacket hp){
+        data = dp;
+        header = hp;
     }
 
-    //TODO: have method allPacketsAccountedFor() returns boolean
-    public boolean allPacketsAccountedFor(){
-        return false;
+    public void addDP(DataPacket dpToAdd){
+        datapackets.put(dpToAdd.packetNumber, dpToAdd);
+        if (lastPacket(dpToAdd)){
+            numPackets = dpToAdd.packetNumber + 1;
+            lastPacket = addedPacket;
+          }
     }
+
+    public boolean allPacketsRecievded(){
+        if(numPackets == null){
+            return false;
+        }
+        return numPackets == datapackets.size() && header != null;
+    }
+
+    public static boolean lastPacket(Packet packet){
+        return packet.status % 4==3;
+    }
+
 
 
 }
