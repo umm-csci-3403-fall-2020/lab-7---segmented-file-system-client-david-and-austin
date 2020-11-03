f9a10670c51bddd7f209e0214f1cd65f8be56718
David <lucht013@morris.umn.edu>
2020-11-03 11:19:39 -0600

    add packetnumber properly

--- src/segmentedfilesystem/DataPacket.java
+++ src/segmentedfilesystem/DataPacket.java
@@ -4,15 +4,15 @@ import java.net.DatagramPacket;
 import java.util.Arrays;
 
 public class DataPacket extends Packet{
-    byte data;
-    byte[] packetNumber;
+    byte[] data;
+    int packetNumber;
 
-    public DataPacket(DatagramPacket rawBytes){
-        status = rawBytes.getData()[0];
-        id = rawBytes.getData()[1];
-        packetNumber[0] = rawBytes.getData()[2];
-        packetNumber[1] = rawBytes.getData()[3];
-        data = Arrays.copyOfRange(rawBytes.getData(),4,rawBytes.getLength());
+    public DataPacket(DatagramPacket recievedPacket){
+        byte[] rawBytes = recievedPacket.getData();
+        status = rawBytes[0];
+        id = rawBytes[1];
+        packetNumber = getPacketNumber(rawBytes);
+        data = Arrays.copyOfRange(recievedPacket.getData(),4,recievedPacket.getLength());
     }
 
     //TODO: Not 100% how this should work, but I'm imagining it just returns the byte that the packet should be holding
@@ -20,4 +20,19 @@ public class DataPacket extends Packet{
         return -1;
     }
 
+    public int getPacketNumber(byte[] bytes) {
+        int number;
+        int primaryByte = bytes[2];
+        int secondaryByte = bytes[3];
+
+        if (primaryByte < 0) {
+            primaryByte += 256;
+        }
+        if (secondaryByte < 0) {
+            secondaryByte += 256;
+        }
+        number = (256 * primaryByte) + secondaryByte;
+
+        return number;
+    }
 }
