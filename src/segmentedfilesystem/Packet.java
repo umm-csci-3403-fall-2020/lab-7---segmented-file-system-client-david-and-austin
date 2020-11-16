90ab1a60d3c529eb77f924cde5f52c492651786e
Austin Robinson <robi1467@morris.umn.edu>
2020-11-15 18:32:13 -0600

    Remove method in Packet that is nolonger needed
    The method was removed from Packet and put into PackMan

--- src/segmentedfilesystem/Packet.java
+++ src/segmentedfilesystem/Packet.java
@@ -5,20 +5,7 @@ import java.util.Arrays;
 abstract class Packet {
     byte status;
     int id;
-    public static Packet sortPacket(byte [] packetBuffer){
-        byte statusByte = packetBuffer[0];
-        byte fileID = packetBuffer[1];
-        boolean isDataPacket = (statusByte%2 ==1);
-        Packet packet;
-        
-        if(isDataPacket){
-            byte[] buffer = Arrays.copyOfRange(packetBuffer, 4, packetBuffer.length);
-            packet = new DataPacket(statusByte, fileID,packetBuffer);
-        }else{
-            packet = new HeaderPacket(packetBuffer);
-        }
-        return packet;
-    }
+  
     public void getFileID(){
 
     }
