318f3c2148ed2472456fee862ab031e58d235722
Austin Robinson <robi1467@morris.umn.edu>
2020-11-19 10:53:56 -0600

    Implement getFileName and refactor name of variable
    created a better fitting variable name inside the header packet constructor

--- src/segmentedfilesystem/HeaderPacket.java
+++ src/segmentedfilesystem/HeaderPacket.java
@@ -15,6 +15,6 @@ public class HeaderPacket extends Packet{
 
     // TODO: 10/27/2020 GetFileName should be able to read ond parse a file name from the Packet object
     public String getFileName(){
-        return "";
+        return filename;
     }
 }
